1f5be9748441c75fc4cc2f9787a8725389c5cf5f
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2024-05-07 11:24:16 +1200

    NEW Use supported modules repo for branch-based logic (#35)

--- funcs.php
+++ funcs.php
@@ -1,180 +1,38 @@
 <?php
 
-// This should always match default branch of silverstripe/framework
-const CURRENT_CMS_MAJOR = 5;
+use SilverStripe\SupportedModules\BranchLogic;
+use SilverStripe\SupportedModules\MetaData;
 
-// List of major branches to not merge up from
-// Add repos in here where the repo was previously unsupported
-// Note these are actual major branches, not CMS major versions
-const DO_NOT_MERGE_UP_FROM_MAJOR = [
-    'silverstripe/silverstripe-linkfield' => '3',
-];
+/**
+ * The path to the composer.json file - note that the action explicitly downloads the composer.json
+ * file for the default branch of the repository and saves it to this path, rather than using
+ * the composer.json file in the "current" branch.
+ */
+const COMPOSER_JSON_PATH = '__composer.json';
 
 function branches(
     string $defaultBranch,
-    string $minimumCmsMajor,
     string $githubRepository,
-    // The following params are purely for unit testing, for the actual github action it will read json files instead
-    string $composerJson = '',
-    string $branchesJson = '',
-    string $tagsJson = ''
-) {
-    if (!is_numeric($defaultBranch)) {
-        throw new Exception('Default branch must be a number');
-    }
-    if (!ctype_digit($minimumCmsMajor)) {
-        throw new Exception('Minimum CMS major must be an integer');
-    }
-
-    // work out default major
-    preg_match('#^([0-9]+)+\.?[0-9]*$#', $defaultBranch, $matches);
-    $defaultMajor = $matches[1];
-    
-    // read __composer.json of the current (default) branch
-    if ($composerJson) {
-        $contents = $composerJson;
-    } elseif (file_exists('__composer.json')) {
-        $contents = file_get_contents('__composer.json');
-    } else {
-        // respository such as silverstripe/eslint-config or silverstripe/gha-auto-tag
-        // make some fake json so that this branch is treated as though it's latest supported major version
-        $contents = json_encode([
-            'require' => [
-                'silverstripe/framework' => '^' . CURRENT_CMS_MAJOR,
-            ],
-        ], JSON_UNESCAPED_SLASHES);
-    }
-
-    $json = json_decode($contents);
-    if (is_null($json)) {
-        $lastError = json_last_error();
-        throw new Exception("Could not parse __composer.json - last error was $lastError");
-    }
-    $defaultCmsMajor = '';
-    $matchedOnBranchThreeLess = false;
-    $version = '';
-    if ($githubRepository === 'silverstripe/developer-docs') {
-        $version = $defaultBranch;
-    }
-    if (!$version) {
-        $version = preg_replace('#[^0-9\.]#', '', $json->require->{'silverstripe/framework'} ?? '');
-    }
-    if (!$version) {
-        $version = preg_replace('#[^0-9\.]#', '', $json->require->{'silverstripe/cms'} ?? '');
-    }
-    if (!$version) {
-        $version = preg_replace('#[^0-9\.]#', '', $json->require->{'silverstripe/mfa'} ?? '');
-    }
-    if (!$version) {
-        $version = preg_replace('#[^0-9\.]#', '', $json->require->{'silverstripe/assets'} ?? '');
-        if ($version) {
-            $matchedOnBranchThreeLess = true;
-        }
-    }
-    if (!$version) {
-        $version = preg_replace('#[^0-9\.]#', '', $json->require->{'cwp/starter-theme'} ?? '');
-        if ($version) {
-            $version += 1;
-        }
-    }
-    if (preg_match('#^([0-9]+)+\.?[0-9]*$#', $version, $matches)) {
-        $defaultCmsMajor = $matches[1];
-        if ($matchedOnBranchThreeLess) {
-            $defaultCmsMajor += 3;
+): array {
+    if (file_exists(COMPOSER_JSON_PATH)) {
+        $contents = file_get_contents(COMPOSER_JSON_PATH);
+        $composerJson = json_decode($contents);
+        if (is_null($composerJson)) {
+            $lastError = json_last_error();
+            throw new Exception('Could not parse ' . COMPOSER_JSON_PATH . " - last error was $lastError");
         }
     } else {
-        $phpVersion = $json->require->{'php'} ?? '';
-        if (substr($phpVersion,0, 4) === '^7.4') {
-            $defaultCmsMajor = 4;
-        } elseif (substr($phpVersion,0, 4) === '^8.1') {
-            $defaultCmsMajor = 5;
-        }
-    }
-    if ($defaultCmsMajor === '') {
-        throw new Exception('Could not work out what the default CMS major version this module uses');
-    }
-    // work out major diff e.g for silverstripe/admin for CMS 5 => 5 - 2 = 3
-    $majorDiff = $defaultCmsMajor - $defaultMajor;
-
-    $minorsWithStableTags = [];
-    $contents = $tagsJson ?: file_get_contents('__tags.json');
-    foreach (json_decode($contents) as $row) {
-        $tag = $row->name;
-        if (!preg_match('#^([0-9]+)\.([0-9]+)\.([0-9]+)$#', $tag, $matches)) {
-            continue;
-        }
-        $major = $matches[1];
-        $minor = $major. '.' . $matches[2];
-        $minorsWithStableTags[$major][$minor] = true;
+        $composerJson = null;
     }
 
-    $branches = [];
-    $contents = $branchesJson ?: file_get_contents('__branches.json');
-    foreach (json_decode($contents) as $row) {
-        $branch = $row->name;
-        // filter out non-standard branches
-        if (!preg_match('#^([0-9]+)+\.?[0-9]*$#', $branch, $matches)) {
-            continue;
-        }
-        // filter out majors that are too old
-        $major = $matches[1];
-        if (($major + $majorDiff) < $minimumCmsMajor) {
-            continue;
-        }
-        // suffix a temporary .999 minor version to major branches so that it's sorted correctly later
-        if (preg_match('#^[0-9]+$#', $branch)) {
-            $branch .= '.999';
-        }
-        $branches[] = $branch;
-    }
-    
-    // sort so that newest is first
-    usort($branches, 'version_compare');
-    $branches = array_reverse($branches);
-    
-    // remove the temporary .999
-    array_walk($branches, function(&$branch) {
-        $branch = preg_replace('#\.999$#', '', $branch);
-    });
-    
-    // remove all branches except:
-    // - the latest major branch in each release line
-    // - the latest minor branch with a stable tag in each release line
-    // - any minor branches without stable tags with a higher minor version than the latest minor with a stable tag
-    $foundMinorInMajor = [];
-    $foundMinorBranchWithStableTag = [];
-    foreach ($branches as $i => $branch) {
-        // only remove minor branches, leave major branches in
-        if (!preg_match('#^([0-9]+)\.[0-9]+$#', $branch, $matches)) {
-            continue;
-        }
-        $major = $matches[1];
-        if (isset($foundMinorBranchWithStableTag[$major]) && isset($foundMinorInMajor[$major])) {
-            unset($branches[$i]);
-            continue;
-        }
-        // for developer-docs which has no tags, pretend that every branch has a tag
-        if (isset($minorsWithStableTags[$major][$branch]) || $githubRepository === 'silverstripe/developer-docs') {
-            $foundMinorBranchWithStableTag[$major] = true;
-        }
-        $foundMinorInMajor[$major] = true;
-    }
-    
-    // remove any branches less than or equal to DO_NOT_MERGE_UP_FROM_MAJOR
-    if (isset(DO_NOT_MERGE_UP_FROM_MAJOR[$githubRepository])) {
-        $doNotMergeUpFromMajor = DO_NOT_MERGE_UP_FROM_MAJOR[$githubRepository];
-        $branches = array_filter($branches, function($branch) use ($doNotMergeUpFromMajor) {
-            return version_compare($branch, "$doNotMergeUpFromMajor.999999.999999", '>');
-        });
-    }
+    $repoMetaData = MetaData::getMetaDataForRepository($githubRepository);
+    $allRepoTags = array_map(fn($x) => $x->name, json_decode(file_get_contents('__tags.json')));
+    $allRepoBranches = array_map(fn($x) => $x->name, json_decode(file_get_contents('__branches.json')));
 
-    // reverse the array so that oldest is first
-    $branches = array_reverse($branches);
-
+    $branches = BranchLogic::getBranchesForMergeUp($githubRepository, $repoMetaData, $defaultBranch, $allRepoTags, $allRepoBranches, $composerJson);
     // max of 6 branches - also update action.yml if you need to increase this limit
     if (count($branches) > 6) {
         throw new Exception('More than 6 branches to merge up. Aborting.');
     }
-    
     return $branches;
 }
--- tests/BranchesTest.php
+++ tests/BranchesTest.php
@@ -10,36 +10,48 @@ class BranchesTest extends TestCase
     public function testBranches(
         array $expected,
         string $defaultBranch,
-        string $minimumCmsMajor,
         string $githubRepository,
         string $composerJson = '',
-        string $branchesJson = '',
-        string $tagsJson = ''
+        string $branchesJson = '[]',
+        string $tagsJson = '[]'
     ) {
         $expectException = $expected === ['__exception__'];
         if ($expectException) {
             $this->expectException(Exception::class);
         }
-        $actual = branches(
-            $defaultBranch,
-            $minimumCmsMajor,
-            $githubRepository,
-            $composerJson,
-            $branchesJson,
-            $tagsJson
-        );
-        if (!$expectException) {
-            $this->assertSame($expected, $actual);
+        try {
+            if ($composerJson) {
+                file_put_contents('__composer.json', $composerJson);
+            }
+            file_put_contents('__branches.json', $branchesJson);
+            file_put_contents('__tags.json', $tagsJson);
+            $actual = branches(
+                $defaultBranch,
+                $githubRepository,
+                $branchesJson,
+                $tagsJson
+            );
+            if (!$expectException) {
+                $this->assertSame($expected, $actual);
+            }
+        } finally {
+            if ($composerJson) {
+                unlink('__composer.json');
+            }
+            unlink('__branches.json');
+            unlink('__tags.json');
         }
     }
 
     public function provideBranches()
     {
+        // Note: Most scenarios are tested upstream in the supported-modules repo.
+        // We just need to check here that we're passing stuff through in an expected way
+        // and any logic/exception unique to this repo
         return [
             '5.1.0-beta1, CMS 6 branch detected on silverstripe/framework' => [
                 'expected' => ['4.13', '4', '5.0', '5.1', '5', '6'],
                 'defaultBranch' => '5',
-                'minimumCmsMajor' => '4',
                 'githubRepository' => 'lorem/ipsum',
                 'composerJson' => <<<EOT
                 {
@@ -76,235 +88,9 @@ class BranchesTest extends TestCase
                 ]
                 EOT,
             ],
-            '5.1.0 stable and match on silverstripe/cms' => [
-                'expected' => ['4.13', '4', '5.1', '5'],
-                'defaultBranch' => '5',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'lorem/ipsum',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "silverstripe/cms": "^5.1"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "3"},
-                    {"name": "3.6"},
-                    {"name": "3.7"},
-                    {"name": "4"},
-                    {"name": "4.10"},
-                    {"name": "4.11"},
-                    {"name": "4.12"},
-                    {"name": "4.13"},
-                    {"name": "5"},
-                    {"name": "5.0"},
-                    {"name": "5.1"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "5.1.0"},
-                    {"name": "5.0.9"},
-                    {"name": "4.13.11"},
-                    {"name": "4.12.11"},
-                    {"name": "4.11.11"},
-                    {"name": "4.10.11"},
-                    {"name": "3.7.4"}
-                ]
-                EOT,
-            ],
-            'match on silverstripe/assets' => [
-                'expected' => ['4.13', '4', '5.1', '5'],
-                'defaultBranch' => '5',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'lorem/ipsum',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "silverstripe/assets": "^2.0"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "4"},
-                    {"name": "4.10"},
-                    {"name": "4.11"},
-                    {"name": "4.12"},
-                    {"name": "4.13"},
-                    {"name": "5"},
-                    {"name": "5.0"},
-                    {"name": "5.1"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "5.1.0"},
-                    {"name": "5.0.9"},
-                    {"name": "4.13.11"},
-                    {"name": "4.12.11"},
-                    {"name": "4.11.11"},
-                    {"name": "4.10.11"}
-                ]
-                EOT,
-            ],
-            'match on silverstripe/mfa' => [
-                'expected' => ['4.13', '4', '5.1', '5'],
-                'defaultBranch' => '5',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'lorem/ipsum',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "silverstripe/mfa": "^5.0"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "4"},
-                    {"name": "4.12"},
-                    {"name": "4.13"},
-                    {"name": "5"},
-                    {"name": "5.0"},
-                    {"name": "5.1"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "5.1.0"},
-                    {"name": "5.0.9"},
-                    {"name": "4.13.11"}
-                ]
-                EOT,
-            ],
-            'Missing `1` branch and match on php version in composer.json' => [
-                'expected' => ['1.13', '2.0', '2.1', '2'],
-                'defaultBranch' => '2',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'lorem/ipsum',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "php": "^8.1"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "1.10"},
-                    {"name": "1.11"},
-                    {"name": "1.12"},
-                    {"name": "1.13"},
-                    {"name": "2"},
-                    {"name": "2.0"},
-                    {"name": "2.1"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "2.1.0-beta1"},
-                    {"name": "2.0.9"},
-                    {"name": "1.13.11"},
-                    {"name": "1.12.11"},
-                    {"name": "1.11.11"},
-                    {"name": "1.10.11"}
-                ]
-                EOT,
-            ],
-            'Two minor branches without stable tags in composer.json' => [
-                'expected' => ['1.13', '1', '2.1', '2.2', '2.3', '2'],
-                'defaultBranch' => '2',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'lorem/ipsum',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "php": "^8.1"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "2"},
-                    {"name": "2.0"},
-                    {"name": "2.1"},
-                    {"name": "2.2"},
-                    {"name": "2.3"},
-                    {"name": "1"},
-                    {"name": "1.13"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "2.3.0-alpha1"},
-                    {"name": "2.2.0-beta1"},
-                    {"name": "2.1.0"},
-                    {"name": "2.0.9"},
-                    {"name": "1.13.11"}
-                ]
-                EOT,
-                ],
-            'Module where default branch has not been changed from CMS 4 and there is a new CMS 6 branch' => [
-                'expected' => ['5.9', '5', '6.0', '6', '7'],
-                'defaultBranch' => '5', // this repo has a `5` branch for CMS 4 and a '6' branch for CMS 5
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'lorem/ipsum',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "php": "^7.4",
-                        "silverstripe/framework": "^4.11"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "7"},
-                    {"name": "6"},
-                    {"name": "6.0"},
-                    {"name": "5"},
-                    {"name": "5.9"},
-                    {"name": "5.8"},
-                    {"name": "5.7"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "6.0.0"},
-                    {"name": "5.9.1"},
-                    {"name": "4.0.1"}
-                ]
-                EOT,
-            ],
-            'developer-docs' => [
-                'expected' => ['4.13', '4', '5.0', '5'],
-                'defaultBranch' => '5',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'silverstripe/developer-docs',
-                'composerJson' => <<<EOT
-                {
-                    "no-require": {}
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "5"},
-                    {"name": "5.0"},
-                    {"name": "4.13"},
-                    {"name": "4.12"},
-                    {"name": "4"},
-                    {"name": "3"}
-                ]
-                EOT,
-                'tagsJson' => '[]',
-            ],
             'More than 6 branches exception' => [
                 'expected' => ['__exception__'],
                 'defaultBranch' => '5',
-                'minimumCmsMajor' => '4',
                 'githubRepository' => 'lorem/ipsum',
                 'composerJson' => <<<EOT
                 {
@@ -340,132 +126,6 @@ class BranchesTest extends TestCase
                     {"name": "4.11.11"},
                     {"name": "4.10.11"},
                     {"name": "3.7.4"}
-                ]
-                EOT,
-            ],
-            'cwp-watea-theme' => [
-                'expected' => ['3.2', '3', '4.0', '4'],
-                'defaultBranch' => '4',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'lorem/ipsum',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "cwp/starter-theme": "^4"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "1"},
-                    {"name": "1.0"},
-                    {"name": "2"},
-                    {"name": "2.0"},
-                    {"name": "3"},
-                    {"name": "3.0"},
-                    {"name": "3.1"},
-                    {"name": "3.2"},
-                    {"name": "4"},
-                    {"name": "4.0"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "4.0.0"},
-                    {"name": "5.0.9"},
-                    {"name": "3.2.0"},
-                    {"name": "3.1.0"},
-                    {"name": "3.0.0"}
-                ]
-                EOT,
-            ],
-            'gha-ci' => [
-                'expected' => ['1.4', '1'],
-                'defaultBranch' => '1',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'silverstripe/gha-ci',
-                'composerJson' => '',
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "1"},
-                    {"name": "1.0"},
-                    {"name": "1.1"},
-                    {"name": "1.2"},
-                    {"name": "1.3"},
-                    {"name": "1.4"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "1.4.0"},
-                    {"name": "1.3.0"},
-                    {"name": "1.2.0"},
-                    {"name": "1.1.0"},
-                    {"name": "1.0.0"}
-                ]
-                EOT,
-            ],
-            'silverstripe-linkfield beta' => [
-                'expected' => ['4.0', '4', '5'],
-                'defaultBranch' => '4',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'silverstripe/silverstripe-linkfield',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "silverstripe/framework": "^5"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "1"},
-                    {"name": "2"},
-                    {"name": "3"},
-                    {"name": "4"},
-                    {"name": "4.0"},
-                    {"name": "5"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "3.0.0-beta1"},
-                    {"name": "2.0.0"},
-                    {"name": "1.0.0"}
-                ]
-                EOT,
-            ],
-            'silverstripe-linkfield stable' => [
-                'expected' => ['4.0', '4', '5'],
-                'defaultBranch' => '4',
-                'minimumCmsMajor' => '4',
-                'githubRepository' => 'silverstripe/silverstripe-linkfield',
-                'composerJson' => <<<EOT
-                {
-                    "require": {
-                        "silverstripe/framework": "^5"
-                    }
-                }
-                EOT,
-                'branchesJson' => <<<EOT
-                [
-                    {"name": "1"},
-                    {"name": "2"},
-                    {"name": "3"},
-                    {"name": "3.0"},
-                    {"name": "3.1"},
-                    {"name": "3.999"},
-                    {"name": "4"},
-                    {"name": "4.0"},
-                    {"name": "5"}
-                ]
-                EOT,
-                'tagsJson' => <<<EOT
-                [
-                    {"name": "4.0.0"},
-                    {"name": "3.0.0"},
-                    {"name": "2.0.0"},
-                    {"name": "1.0.0"}
                 ]
                 EOT,
             ],
