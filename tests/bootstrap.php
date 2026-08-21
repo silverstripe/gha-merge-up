52c6fd26b25ffbb5391726af1d1f52fd36612856
Steve Boyd <emteknetnz@gmail.com>
2026-08-21 14:03:47 +1200

    ENH Use yarn 4 (#67)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
+
+use SilverStripe\SupportedModules\MetaData;
+
 // working directory will be root
 include 'funcs.php';
+
+// Read repositories.json from the installed package instead of fetching it over the network
+MetaData::$isRunningUnitTests = true;
